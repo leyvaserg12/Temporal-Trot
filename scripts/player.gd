ae85e5d559ef46006498216d2641cbbfdb8de750
Jack Donegan <lvf165@usask.ca>
2024-11-02 14:59:25 -0600

    autoscrolled the platformer

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,13 @@
+class_name Player
+
 extends CharacterBody2D
 
+@export var camera: Camera2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var camera_speed = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,8 +22,14 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED + camera_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, camera_speed, SPEED)
 
 	move_and_slide()
+
+
+func _on_camera_speed_update(cspeed) -> void:
+	camera_speed = cspeed
+	print(cspeed)
+	pass # Replace with function body.
