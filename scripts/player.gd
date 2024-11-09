410869bba60cdcfad6cd9d65d83e0993a257485b
Jack Donegan <lvf165@usask.ca>
2024-11-08 19:21:23 -0600

    moved obstacles around to be a bit more interesting

--- scripts/player.gd
+++ scripts/player.gd
@@ -33,14 +33,15 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("mv_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	# Move the dude.
 	var direction := Input.get_axis("mv_left", "mv_right")
 	if direction:
 		velocity.x = direction * SPEED + camera.speed
 	else:
 		velocity.x = move_toward(velocity.x, camera.speed, SPEED)
 	
-	# kills you if you fall off screen or fall behind camera
+	# Kills you dead if you fall off camera.
 	if (position.y > camera.get_screen_center_position().y + window_size.y/2/camera.zoom.y or
 		position.x < camera.get_screen_center_position().x - window_size.x/2/camera.zoom.x):
 		game.player_collided.emit()
