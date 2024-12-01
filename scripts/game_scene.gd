b6b5763cb036f9684b6235a8d1ba62a64fbf9708
Jack Donegan <lvf165@usask.ca>
2024-11-30 22:16:28 -0600

    restructed game_scene to support new setpiece layout

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -4,15 +4,14 @@ extends Node2D
 
 @onready var player: Player = $Player
 @onready var timer: Timer = $DeathTimer
-@onready var present: Node2D = $Present
-@onready var future: Node2D = $Future
 @onready var pparallax: CanvasLayer = $PresentParallax
 @onready var fparallax: CanvasLayer = $FutureParallax
+@onready var terrain: Node = $Terrain
 
-enum {PRESENT, FUTURE}
+enum DIM {PRESENT, FUTURE}
 enum STATE {RUNNING, GAMEOVER}
 
-var current_dimension = PRESENT
+var current_dimension = DIM.PRESENT
 var state: STATE = STATE.RUNNING
 
 # seperate signals, just in case the player somehow survives
@@ -21,8 +20,8 @@ signal player_died
 
 func _ready() -> void:
 	# make sure the present and future are in the right state
-	toggle_dimension(present, true)
-	toggle_dimension(future, false)
+	toggle_dimension(DIM.PRESENT, true)
+	toggle_dimension(DIM.FUTURE, false)
 	
 	timer.timeout.connect(reload)
 	player_collided.connect(player_dies)
@@ -32,25 +31,27 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("mv_tele"):
 		teleport()
 
-func toggle_dimension(dim: Node2D, vis: bool):
-	dim.visible = vis
-	var layer = 1 if vis else 0
-	
-	dim.get_node("Terrain").tile_set.set_physics_layer_collision_layer(0, layer)
+func toggle_dimension(dim: DIM, vis: bool):
+	var str = "Present" if dim == DIM.PRESENT else "Future"
+	for piece in terrain.get_children():
+		piece.get_node(str).visible = vis
+		var layer = 1 if vis else 0
+		
+		piece.get_node(str).get_node("Terrain").tile_set.set_physics_layer_collision_layer(0, layer)
 
 func teleport():
-	if current_dimension == PRESENT:
-		current_dimension = FUTURE
+	if current_dimension == DIM.PRESENT:
+		current_dimension = DIM.FUTURE
 		
-		toggle_dimension(future, true)
+		toggle_dimension(DIM.FUTURE, true)
 		fparallax.visible = true
-		toggle_dimension(present, false)
+		toggle_dimension(DIM.PRESENT, false)
 		pparallax.visible = false
 	else:
-		current_dimension = PRESENT
-		toggle_dimension(present, true)
+		current_dimension = DIM.PRESENT
+		toggle_dimension(DIM.PRESENT, true)
 		pparallax.visible = true
-		toggle_dimension(future, false)
+		toggle_dimension(DIM.FUTURE, false)
 		fparallax.visible = false
 
 func player_dies():
