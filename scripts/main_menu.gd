6d7e53094b59cbafb59baf88da17ab32b09f1c86
Serg <leyvaserg12@gmail.com>
2024-12-04 03:00:30 -0600

    added transition to play button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,24 +1,13 @@
 extends Node2D
 
-#func _ready() -> void:
-#	$FadeTransition/AnimationPlayer.play("RESET")
+@onready var transition = $Transition
 
 func _on_start_pressed() -> void:
-	# the fade transition doesn't work, so it just looks like a big stutter 
-	# before the game starts
+	transition.play("fade_out")
 	
-	#$FadeTransition.show()
-	#$FadeTransition/fade_timer.start()
-	#$FadeTransition/AnimationPlayer.play("fade_in")
-	get_tree().change_scene_to_file("res://scripts/story_scenes.tscn")
-
 func _on_instructions_pressed() -> void:
 	$Menu/Instructions.visible = true
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-#func _on_fade_timer_timeout() -> void:
-#	if button_type == "start":
-#		get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
-		
+# changes to story scene after animation
+func _on_transition_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://scenes/story_scenes.tscn")
--- story_scenes.gd
+++ story_scenes.gd
@@ -1,25 +1,28 @@
 extends Node2D
 
+@onready var transition =  $Transition
+
 var sceneNumber = 1
 var currentScene = "res://assets/textures/ui/story_scenes/scene1.png"
+
 func _ready() -> void:
-	pass # Replace with function body.
+	transition.play("fade_in")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 		# when mouse is pressed go to next scene
 		if Input.is_action_just_pressed("mv_jump"):
-			#check if we're on last scene and go to game play
+			
+			#check if we're on last scene and go to game play after next
 			sceneNumber += 1
-			if sceneNumber == 5:
+			if sceneNumber > 5:
 				get_tree().change_scene_to_file("res://scenes/game_scene.tscn")	
 			currentScene = _switch_scenes(sceneNumber)
-		$scene.texture = load(currentScene)
+		$scenes/sceneTexture.texture = load(currentScene)
 		
 
 # function to switch to next scene / switch texture of scene
 func _switch_scenes(scene_number) -> String:
 	var newScene = "res://assets/textures/ui/story_scenes/scene"+ str(scene_number) + ".png"
-	print(newScene)
 	return newScene
